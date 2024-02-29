4933bde5cf4b7c50ac0b0ac79e3f312a5168090f
ali fares <alifares11946@example.com>
2024-02-29 12:25:03 +0200

    first

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import Link from 'next/link'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Image from 'next/image'
+import { HeaderNav } from '../Nav'
+import MobileNav from '../MobileNav'
+
+import classes from './index.module.scss'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathName = usePathname()
+  return (
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathName) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={[classes.wrap].filter(Boolean).join(' ')}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+        </Link>
+
+        <HeaderNav header={header} />
+        {/* <MobileNav header={header} /> */}
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
